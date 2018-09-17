1be39b5cbef9793a889a243956ef8f9bbd99cc1e
HOME <HOME@HOME-PC>
2018-09-17 10:17:46 +0900

    p0917 commit message

--- p0917/src/p0917/Helloworld.java
+++ p0917/src/p0917/Helloworld.java
@@ -4,5 +4,7 @@ public class Helloworld {
 
 	public static void main(String[] args) {
 		System.out.println("HelloWorld!!!");
+		System.out.println("헬로월드!!!");
+		System.out.println("헬로월드22!!!");
 	}
 }
